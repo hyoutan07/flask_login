067eeca2aa5e18b645c2ad99013534feebda2500
hyoutan07 <hyoutan0726@gmail.com>
2022-03-15 11:41:13 +0900

    go]

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 
 
 app = Flask(__name__)
@@ -11,3 +11,14 @@ db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(50), nullable=False, unique=True)
+	password = db.Column(db.String(25))
+
+
+@app.route('/')
+def blog():
+    return render_template('index.html')
+
